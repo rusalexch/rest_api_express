6a41da8870cfbf8225a6de884e8d79f175e819c5
Alexander Churkin <alex.asutp@gmail.com>
2020-11-06 14:56:28 +0300

    feat: added cors

--- app/src/server.js
+++ app/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const path = require('path');
+const cors = require('cors');
 
 const { APP_PORT: port } = process.env;
 const db = require('./core/database');
 
 const app = express();
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
