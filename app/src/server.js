e559f9c0c2642dccfaae03212e53eee2c00ecec3
Alexander Churkin <alex.asutp@gmail.com>
2020-11-06 18:42:00 +0300

    feat: added body-parser

--- app/src/server.js
+++ app/src/server.js
@@ -2,18 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const user = require('./modules/users/index');
+const bodyParser = require('body-parser');
 
 const { APP_PORT: port } = process.env;
 const db = require('./core/database');
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
 });
 
-user(db.clien)
+user(db.clien);
 
 app.listen(port, () => {
   console.info(`Server runing at http://localhost:${port}`)
